a22f1fd29a5ba631edd5f3b7085d6be53c8ee1a9
symnbe <ookaminingen@me.com>
2023-04-05 08:50:32 +0000

    [Add]bean/new_html + routing + beans_controller

--- app/controllers/beans_controller.rb
+++ app/controllers/beans_controller.rb
@@ -1,12 +1,25 @@
 class BeansController < ApplicationController
-  def index
-    @beans - Bean.all
+  
+  def new
+    @bean = Bean.new
   end
   
+  def create
+    bean = Bean.new(bean_params)
+    bean.save
+    redirect_to beans
+  end
+  
+  def index
+    @beans = Bean.all
+  end
+
   def show
     @bean = Bean.find(params[:id])
   end
 
   def edit
   end
+  
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   
   devise_for :users
   root to: 'homes#top'
-  resources:beans, only:[:index, :show, :edit, :update, :destroy]
+  resources:beans, only:[:new, :create, :index, :show, :edit, :update, :destroy]
   
   
 end
