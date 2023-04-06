9c3aaf48fd5b89611b127ef21aff25ec93a04575
symnbe <ookaminingen@me.com>
2023-04-06 11:59:21 +0000

    [fix]bean_image_at_model

--- app/models/bean.rb
+++ app/models/bean.rb
@@ -4,10 +4,18 @@ class Bean < ApplicationRecord
 
   belongs_to :user
 
-  validates :store_name, presence :true
-  validates :opinion, presence :true
+  validates :store_name, presence: true
+  validates :opinion, presence: true
 
   enum production_area: { brazil: 0, vietnam: 1, columbia: 2, indonesia: 3, ethiopia: 4, honduras: 5, india: 6, peru: 7, guatemala: 8, uganda: 9 }
   enum degree_of_roasting: { light_roast: 0, cinnamon_roast: 1, medium_roast: 2, high_roast: 3, city_roast: 4, fullcity_roast: 5, french_roast: 6, italian_roast: 7}
+
+  def get_bean_image
+    unless bean_image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image_unsplash.jpg')
+      bean_image.attach(io: File.open(file_path), filename: 'no_image_unsplash.jpg', content_type: 'image/jpeg')
+    end
+    bean_image
+  end
 
 end
