c54dd43bb251c542053166566e9de44fa6ebd248
symnbe <ookaminingen@me.com>
2023-05-08 23:16:17 +0000

    [fin]beans/show.html.erb/非同期

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,17 +1,17 @@
 class FavoritesController < ApplicationController
 
   def create
-    bean = Bean.find(params[:bean_id])
-    favorite = current_user.favorites.new(bean_id: bean.id)
+    @bean = Bean.find(params[:bean_id])
+    favorite = current_user.favorites.new(bean_id: @bean.id)
     favorite.save
-    redirect_to bean_path(bean)
+    # redirect_to bean_path(bean)
   end
 
   def destroy
-    bean = Bean.find(params[:bean_id])
-    favorite = current_user.favorites.find_by(bean_id: bean.id)
+    @bean = Bean.find(params[:bean_id])
+    favorite = current_user.favorites.find_by(bean_id: @bean.id)
     favorite.destroy
-    redirect_to bean_path(bean)
+    # redirect_to bean_path(bean)
   end
 
 end
